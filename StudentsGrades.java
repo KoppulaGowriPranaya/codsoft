407d5d01b30a2502f4f52ae08921d7d881cb8844
KoppulaGowriPranaya <gowripranayakoppula@gmail.com>
2024-08-20 20:40:42 +0530

    Add files via upload

---
+++ StudentsGrades.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class StudentsGrades
+{
+    
+public class StudentGrades {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input for the number of subjects
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        // Array to store marks for each subject
+        int[] marks = new int[numSubjects];
+        int totalMarks = 0;
+
+        // Taking input for marks obtained in each subject
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+            totalMarks += marks[i];
+        }
+
+        // Calculating average percentage
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        // Calculating grade
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        // Displaying results
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
+}
